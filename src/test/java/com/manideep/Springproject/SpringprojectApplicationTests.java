fd94af861774c3fef9022429339ddbffc2f1c5b8
Manideepalapati <manideepalapati2005@gmail.com>
2023-05-22 16:25:17 +0530

    first commit

---
+++ src/test/java/com/manideep/Springproject/SpringprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manideep.Springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
